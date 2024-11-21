6c709e77acd3dbcaef1b1a4c1ed2059c5378d442
Radek Sienkiewicz <mail@velvetshark.com>
2024-11-21 16:49:51 +0100

    Disable ESLint in build stage

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // Disable ESLint during build
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Disable TypeScript checks during build
+    ignoreBuildErrors: true,
+  },
+}
+
+module.exports = nextConfig
